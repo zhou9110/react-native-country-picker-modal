f8ed4db40d60c8af45ec6f39cee8483270b639a8
Xavier Carpentier <xcapetir@gmail.com>
2019-12-27 11:40:05 +0100

    fix force emoji

--- src/FlagButton.tsx
+++ src/FlagButton.tsx
@@ -3,7 +3,6 @@ import {
   TouchableOpacity,
   StyleSheet,
   View,
-  Platform,
   StyleProp,
   ViewStyle,
   TextProps,
@@ -154,7 +153,7 @@ export const FlagButton = ({
 }
 
 FlagButton.defaultProps = {
-  withEmoji: Platform.OS === 'ios',
+  withEmoji: true,
   withCountryNameButton: false,
   withCallingCodeButton: false,
   withCurrencyButton: false,
